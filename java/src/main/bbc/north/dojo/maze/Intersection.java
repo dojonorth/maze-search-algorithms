12eccb6789af9009c7baec2b7d842429d3f5ce52
James Murphy <jmurphy84@googlemail.com>
2015-07-30 22:26:52 +0100

    Java: Building generateMaze function for Braid algorithm

---
+++ java/src/main/bbc/north/dojo/maze/Intersection.java
@@ -0,0 +1,14 @@
+package bbc.north.dojo.maze;
+
+/**
+ * Created by jamesmurphy on 30/07/2015.
+ */
+public class Intersection {
+    private final int x;
+    private final int y;
+
+    public Intersection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- java/src/main/bbc/north/dojo/maze/generator/BraidMazeGenerator.java
+++ java/src/main/bbc/north/dojo/maze/generator/BraidMazeGenerator.java
@@ -1,7 +1,8 @@
 package bbc.north.dojo.maze.generator;
 
-import java.util.Arrays;
-import java.util.Collections;
+import bbc.north.dojo.maze.Intersection;
+
+import java.util.*;
 
 public class BraidMazeGenerator extends DefaultMazeGenerator {
 
@@ -12,11 +13,20 @@ public class BraidMazeGenerator extends DefaultMazeGenerator {
     @Override
     public int[][] generateMaze(int cx, int cy) {
         maze = recursiveBacktrack(cx, cy);
+        // 1. Create traversal graph
         traversal = populateTraversalGraph(maze, traversal);
 
-        int current = maze[x - 1] [y - 1];
+        // 2.Start at entrance (get the entrance cell)
+        int cx = x - 1,
+            cy = y - 1;
+
+        int current = maze[cx][cy];
         int entrance = current;
         DIR next;
+
+        List<Intersection> toVisit = new ArrayList<>();
+
+        // 3.If there is one option take it
         if (hasOneAvailableRoute(current)) {
             if (current == 14) { // only available is north
                 next = DIR.N;
@@ -24,21 +34,29 @@ public class BraidMazeGenerator extends DefaultMazeGenerator {
                 next = DIR.W;
             }
         } else if (hasTwoAvailableRoutes(current)) {
+            // 4.Else pick an untraversed option
             DIR[] availableDirs = new DIR[]{ DIR.N, DIR.W };
             next = selectRandomDirection(availableDirs);
-            // reduce the number of directions
-            traversal[x - 1][y - 1] -= DIR.;
+            // 5.Reduce the number of options by 1
+            traversal[cx][cy] -= next.bit;
+            // 6.Store a reference to this cell in the 'toVisit' queue
+            toVisit.add(new Intersection(cx, cy));
+        } else if (isDeadEnd(current)) {
+            // 8.Are we at a dead end? i.e. there no options?
+            // 9a.Yes - Remove a random wall but not a side wall
+            // if cy == 0 -- south wall
+            // if cx == 0 -- west wall
+            // if cy + 1 % maze[0].length == 0 -- east wall
+            // if cx + 1 % maze.length == 0 -- north wall
+
+//            int
+            if (cy == 0) {
+
+            }
         }
 
-        // 1.Start at entrance (get the entrance cell)
-        // 2.Check all directions for walls
-        // 3.If there is one option take it
-        // 4.Else pick an untraversed option
-        // 5.Reduce the number of options by 1
-        // 6.Store a reference to this cell in the 'toVisit' queue
         // 7.Remove from the 'toVisit' queue
-        // 8.Are we at a dead end? i.e. there no options?
-        // 9a.Yes - Remove a random wall but not a side wall
+
         // 9b.Mark current cell as traversed - remove cell from 'toVisit' queue
         // 10.Is toVisit queue empty?
         // 10a.Yes END
@@ -46,6 +64,10 @@ public class BraidMazeGenerator extends DefaultMazeGenerator {
         // 10c.Continue from 2.
         // 9c.No get Continue to 2
         return null;
+    }
+
+    private boolean isDeadEnd(int cell) {
+        return cell == TRAVERSAL.DEAD_END.bit;
     }
 
     private DIR selectRandomDirection(DIR[] available) {
