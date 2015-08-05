9f53ca02e7aa4f3fca6237bfd11923d11b186f40
James Murphy <jmurphy84@googlemail.com>
2015-08-05 14:33:46 +0100

    Java: Added Path object

---
+++ java/src/main/bbc/north/dojo/maze/Path.java
@@ -0,0 +1,12 @@
+package bbc.north.dojo.maze;
+
+public class Path {
+
+    public final int x;
+    public final int y;
+
+    public Path(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
