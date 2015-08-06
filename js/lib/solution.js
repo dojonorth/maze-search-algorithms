3a8ff7f2f4192b116afcaec7f730d8d67e73865f
subvertnormality <andy@subvertnormality.com>
2015-08-06 22:56:14 +0100

    Adding some help comments

--- js/lib/solution.js
+++ js/lib/solution.js
@@ -1,9 +1,21 @@
+
+// Contains utilities to get the available open directions from a cell and to check if a cell is the exit
+var helpers = require('./helpers');
+
 module.exports = function(maze) {
 
-  // Takes an array of [x, y] coordinates for visited cells in the solution.
-  // Example precanned solution path for precannedMaze1
+  // maze.representation contains a two dimensional array of cells making up the representation of the maze.
 
+  // You need to return an array of [x, y] coordinates for visited cells in the solution.
+  // Each time you visit a cell, you must push it to the array.
+  // The last item in the array will be the [x, y] of the exit tile.
+
+  // Example precanned solution path for precannedMaze1
   // return [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8],[0,9],[0,10],[0,11],[0,12],[0,13],[0,14],[1,14],[2,14],[3,14],[3,13],[4,13],[5,13],[5,12],[4,12],[4,11],[3,11],[3,10],[4,10],[5,10],[6,10],[6,11],[6,12],[6,13],[6,14],[7,14],[8,14],[8,13],[8,12],[9,12],[10,12],[11,12],[12,12],[13,12],[14,12],[14,13],[14,14]];
 
-  return [];
+  var solution = [];
+
+
+
+  return solution;
 }
